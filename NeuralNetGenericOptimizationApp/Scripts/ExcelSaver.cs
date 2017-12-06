9ea5b9b1d7800d77f51553ca4deb6711f5dd9158
Saper <sawek111@gmail.com>
2017-12-07 00:52:40 +0100

    <ExcelSaver> only interface

---
+++ NeuralNetGenericOptimizationApp/Scripts/ExcelSaver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetGenericOptimizationApp.Scripts
+{
+    public class ExcelSaver 
+    {
+        public ExcelSaver(string path)
+        {
+
+        }
+
+        public void Save(params string[] data)
+        {
+
+            return;
+        }
+    }
+}
