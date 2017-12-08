e03e62cd8e4fbf52da1eb356491065f1a118e66b
Saper <sawek111@gmail.com>
2017-12-08 22:05:55 +0100

    <LocalSeacrch>

--- NeuralNetGenericOptimizationApp/Scripts/Alghorithm/LocalSearchAlghorithm.cs
+++ NeuralNetGenericOptimizationApp/Scripts/Alghorithm/LocalSearchAlghorithm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NeuralNetGenericOptimizationApp.Scripts.GeneticAlghoritm;
+using NeuralNetGenericOptimizationApp.Scripts.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,75 @@ using System.Threading.Tasks;
 
 namespace NeuralNetGenericOptimizationApp.Scripts.Alghorithm
 {
-    class LocalSearchAlghorithm
+    public class LocalSearchAlghorithm
     {
+        public Individual CheckNeighbourhood(Individual[] individuals, int deep)
+        {
+            Individual best = individuals[0];
+            for (int i = 0; i < individuals.Length; i++)
+            {
+                Individual localBest = SearchNeighbourhood(individuals[i], deep);
+                best = Individual.GetBetterIndividual(best, localBest);
+            }
 
+            return best;
+        }
+
+        private Individual SearchNeighbourhood(Individual individual, int deep)
+        {
+            Individual best = individual;
+            for(int chromosomes  = 0; chromosomes <  Enum.GetNames(typeof(ChromosomeType)).Length; chromosomes++)
+            {
+                Individual additiveOffsetNeighbour = SearchOneSideNeighbourhood(individual, (ChromosomeType)chromosomes, deep, +1);
+                best = Individual.GetBetterIndividual(best, additiveOffsetNeighbour);
+                Individual negativeOffsetNeighbour = SearchOneSideNeighbourhood(individual, (ChromosomeType)chromosomes, deep, -1);
+                best = Individual.GetBetterIndividual(best, negativeOffsetNeighbour);
+            }
+
+            return best;
+        }
+
+        private Individual SearchOneSideNeighbourhood(Individual individual, ChromosomeType type, int deep, int step)
+        {
+            int counter = 0;
+            int offset = 0;
+            Individual lastBest = individual;
+
+            while (counter <= deep)
+            {
+                offset += step;
+                counter++;
+                Individual newNeighbour = MakeNeighbour(individual, type, offset);
+                if(CanEndLocalSearch(lastBest, newNeighbour))
+                {
+                    break;
+                }
+                lastBest = newNeighbour;
+            }
+
+            return lastBest;
+        }
+
+        private Individual MakeNeighbour(Individual origin, ChromosomeType type, int offset )
+        {
+            int testedValue = origin.GetChromosome(type).GetGeneDecimalValue() + offset;
+            if(testedValue < 0)
+            {
+                return null;
+            }
+            Individual neighbour = new Individual(origin, type, testedValue);
+
+            return neighbour;
+        }
+
+        private bool CanEndLocalSearch(Individual lastbest, Individual newIndividual)
+        {
+            if(newIndividual == null)
+            {
+                return true;
+            }
+
+            return lastbest.GetFitness() >= newIndividual.GetFitness();
+        }
     }
 }
