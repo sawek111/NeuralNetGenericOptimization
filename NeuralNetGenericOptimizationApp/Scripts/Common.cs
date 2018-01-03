fc48e730caa17bb18ff83bbd4acc987cb1bf97a4
Paulina <Paulina@Lenovo>
2018-01-03 23:31:01 +0100

    <Common> loading textbox to values' array

--- NeuralNetGenericOptimizationApp/Scripts/Common.cs
+++ NeuralNetGenericOptimizationApp/Scripts/Common.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace NeuralNetGenericOptimizationApp.Scripts.Utils
 {
@@ -35,6 +36,27 @@ namespace NeuralNetGenericOptimizationApp.Scripts.Utils
                 left -= curr;
             }
             yield return left;
+        }
+
+        /// <summary>
+        /// Loads all values with given type separated with comma (,)
+        /// </summary>
+        /// <param name="textbox"> Textbox to read values from</param>
+        /// <returns>Array with readed values </returns>
+        public static T[] LoadValuesArrayFromTextbox<T>(TextBox textbox)
+        {
+            string[] words = textbox.Text.Split(',');
+            List<T> wordsList = new List<T>();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                if(words[i] is T)
+                {
+                    wordsList.Add((T)Convert.ChangeType(words[i], typeof(T)));
+                }
+            }
+
+            return wordsList.ToArray();
         }
 
         public static string ConvertPathToR(string CSharpPath)
