9abacf8e0f2f447135770f368d5ec66396ceacf3
Paulina <Paulina@Lenovo>
2018-01-03 23:31:50 +0100

    <Optimizer> one line calling all variants

--- NeuralNetGenericOptimizationApp/Scripts/Optimizer.cs
+++ NeuralNetGenericOptimizationApp/Scripts/Optimizer.cs
@@ -12,6 +12,13 @@ namespace NeuralNetGenericOptimizationApp.Scripts
 {
     public class Optimizer
     {
+
+        public void Calculate()
+        {
+
+        }
+
+
         public void RandomSearch()
         {
             RandomSearchAlghorithm randomSearchAlghorithm = new RandomSearchAlghorithm(4);
@@ -20,26 +27,63 @@ namespace NeuralNetGenericOptimizationApp.Scripts
             return;
         }
 
-        public void MemeticSearch(int Generations, int GenerationSize, int neighbourhoodSize)
+        public float MemeticSearch(int[] generationsArray, int[] generationSizeArray, int[] neighbourhoodSizeArray, float[] mutationRateArray)
         {
+            float time = 0.0f;
+            for(int crossing = 0; crossing < Enum.GetNames(typeof(CrossingType)).Length; crossing++)
+            {
+                for(int selection = 0; selection < Enum.GetNames(typeof(SelectionType)).Length; selection++)
+                {
+                    
+                    CalculateMemeticSearchForAllParameters((SelectionType)selection,(CrossingType)crossing, generationsArray, generationSizeArray, mutationRateArray, neighbourhoodSizeArray);
+                }
+            }
+
+            return time;
+        }
+
+        private void CalculateMemeticSearchForAllParameters(SelectionType selection, CrossingType crossing, int[] GenerationsArray, int[] GenerationSizeArray, float[] mutationRateArray, int[] neighbourhoodSizeArray)
+        {
+            for(int generations = 0; generations < GenerationsArray.Length; generations++)
+            {
+                for(int generationSize = 0; generationSize < GenerationSizeArray.Length; generationSize++)
+                {
+                    for(int mutationRate = 0; mutationRate < mutationRateArray.Length; mutationRate++)
+                    {
+                        for(int neighbourhoodSize = 0; neighbourhoodSize < neighbourhoodSizeArray.Length; neighbourhoodSize++)
+                        {
+                            CalculateMemetic(selection, crossing, generations, generationSize, mutationRate, neighbourhoodSize, true);
+                            CalculateMemetic(selection, crossing, generations, generationSize, mutationRate, neighbourhoodSize, false);
+                            //TODO SAVE TO file 
+                        }
+                    }
+                }
+            }
+        }
+
+        private float CalculateMemetic(SelectionType selection, CrossingType crossing, int generations, int generationSize , float mutationRate, int neighbourhoodSize, bool elitism )
+        {
+
             GeneticAlgorithm genetic = new GeneticAlgorithm();
             LocalSearchAlghorithm localSearch = new LocalSearchAlghorithm();
 
-            Population population = new Population(GenerationSize);
+            Population population = new Population(generationSize);
             Individual best = new Individual();
             int generationNr = 0;
 
-            while (generationNr < Generations)
+            while (generationNr < generations)
             {
                 generationNr++;
-                population = genetic.Evolve(population, SelectionType.Rank, CrossingType.SinglePoint, elitism: true);
-                Individual localBest =  localSearch.CheckNeighbourhood(population.GetRepresentation(5), 5);
+                population = genetic.Evolve(population, selection, crossing, mutationRate, elitism);
+                Individual localBest = localSearch.CheckNeighbourhood(population.GetRepresentation(5), neighbourhoodSize);
                 best = Individual.GetBetterIndividual(localBest, best);
             }
             best = Individual.GetBetterIndividual(population.GetMostAccurant(1)[0], best);
 
-            return;
+            return best.GetFitness();
         }
+
+        // private void 
 
     }
 }
